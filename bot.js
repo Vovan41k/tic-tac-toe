476f2d8a067d3fe74408c5141d5869a6a06c4113
Vovan41k <fatuev_vladimir@mail.ru>
2022-07-20 11:06:30 +0300

    version 1.0.1

--- bot.js
+++ bot.js
@@ -1,116 +1,40 @@
 require('dotenv').config()
 const token = process.env.TOKEN
 const TelegramBot = require('node-telegram-bot-api');
+const { reset,
+    getScene,
+    wins, 
+    checkIfWin,
+    getFreeRandomCoords, 
+    getPoints } = require('./lib/ttt');
+const { sendTurn, setBot } = require('./lib/tttTg');
+const { random } = require('./lib/utils');
 
 const bot = new TelegramBot(token, { polling: true });
+setBot(bot)
 
-
-let scene = [
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0],
-]
-const wins = [
-    [[0, 0], [0, 1], [0, 2]],
-    [[1, 0], [1, 1], [1, 2]],
-    [[2, 0], [2, 1], [2, 2]],
-    [[0, 0], [1, 0], [2, 0]],
-    [[0, 1], [1, 1], [2, 1]],
-    [[0, 2], [1, 2], [2, 2]],
-    [[0, 0], [1, 1], [2, 2]],
-    [[2, 0], [1, 1], [0, 2]],
-]
-const signs = ['⬜', '❌', '⚪']
-const reset = () => {
-    scene = [
-        [0, 0, 0],
-        [0, 0, 0],
-        [0, 0, 0],
-    ]
-}
-let userPoints = 0
-let botPoints = 0
-const getPoints = () => {
-    return `общий счет игры  (пользователь:бот) ${userPoints}:${botPoints}`
-}
-const renderButtons = (scene, sign, prefix='t') => {
-    return scene.map((row, indexX)=>{
-        return scene[indexX].map((num, indexY) => {
-            return {
-                text: signs[scene[indexX][indexY]],
-                callback_data: `${prefix},${indexX},${indexY},` + sign,
-            }
-        })
-    })
-}
-const random = (num) => {
-    const res = Math.floor(Math.random() * num)
-    return res
-}
-const getFreeRandomCoords = (scene) => {
-    let freePlaces = 0
-    for (let x = 0; x < scene.length; x += 1) {
-        for (let y = 0; y < scene[x].length; y += 1) {
-            if (scene[x][y] === 0) {
-                freePlaces += 1
-            }
-        }
-    }
-    if (freePlaces === 0) {
-        return [-1, -1]
-    }
-    let freeIndex = random(freePlaces)
-    for (let x = 0; x < scene.length; x += 1) {
-        for (let y = 0; y < scene[x].length; y += 1) {
-            if (scene[x][y] === 0) {
-                if (freeIndex === 0) {
-                    return [x, y]
-                }
-                freeIndex -= 1
-            }
-        }
-    }
-}
-const checkIfWin = (scene, wins, sign) => {
-    console.log(scene, wins, sign)
-    for (let win of wins) {
-        let points = 0
-        for (let coords of win) {
-            const [x, y] = coords
-            if (scene[x][y] === sign) {
-                points += 1
-            }
-        }
-        if (points === 3) {
-            return true
-        }
-    }
-    return false
-}
-
-let player1 
-let player2 
-
-
+let player1
+let player2
 
 bot.onText(/\/play/, (msg, match) => {
-    reset()
-    if (msg.chat.id === +process.env.ID_VLADIMIR){
-        player1 = +process.env.ID_VLADIMIR
-        player2 = +process.env.ID_VASILIY
-    }
-    else {
-        player2 = +process.env.ID_VLADIMIR
-        player1 = +process.env.ID_VASILIY
-    }
-    bot.sendMessage(player1, 'Ваш ход',
-        {
-            "reply_markup": {
-                "inline_keyboard":renderButtons(scene, 1, 'p')
-            },
-        })
+    try {
+        reset()
+        const scene = getScene()
+        if (msg.chat.id === +process.env.ID_VLADIMIR) {
+            player1 = +process.env.ID_VLADIMIR
+            player2 = +process.env.ID_VASILIY
+        }
+        else {
+            player2 = +process.env.ID_VLADIMIR
+            player1 = +process.env.ID_VASILIY
+        }
+        sendTurn(player1, 'Ваш ход', scene, 1, 'p')
+
         bot.sendMessage(player2, 'Ваш соперник совершает ход')
-    })
+    } catch (error) {
+        console.log(error)
+    }
+})
 
 
 bot.onText(/\/start/, (msg, match) => {
@@ -135,26 +59,15 @@ bot.onText(/\/start/, (msg, match) => {
 bot.onText(/\/ttt/, (msg, match) => {
     try {
         reset()
+        const scene = getScene()
         const chatId = msg.chat.id;
         if (Math.random() < 0.5) {
             //ход пользователя
-            bot.sendMessage(chatId, 'Выполните ход!', {
-                "reply_markup": {
-                    "inline_keyboard": renderButtons(scene, 1)
-
-                },
-
-            });
+            sendTurn(chatId, 'Выполните ход!', scene, 1)
         } else {
             const [botX, botY] = getFreeRandomCoords(scene)
             scene[botX][botY] = 1
-            bot.sendMessage(chatId, 'Выполните ход!', {
-                "reply_markup": {
-                    "inline_keyboard": renderButtons(scene, 2)
-
-                },
-
-            });
+            sendTurn(chatId, 'Выполните ход!', scene, 2)
         }
     } catch (error) {
         console.log(error)
@@ -164,67 +77,36 @@ bot.onText(/\/ttt/, (msg, match) => {
 bot.on('callback_query', (query) => {
     console.log(query.from.id, query.data)
     try {
-
+        const scene = getScene()
         const chatId = query.from.id
         const [prefix] = query.data.split(',')
-        
+
         if (prefix === 'p') {
             const [, x, y, strsign] = query.data.split(',')
+            const sign = +strsign
             if (scene[x][y] !== 0) {
                 bot.sendMessage(chatId, 'Так ходить нельзя, выберите другую клетку')
                 return false
             }
-            const sign = +strsign
             scene[x][y] = sign
-            const currentPlayer = chatId    
-            const anotherPlayer = (currentPlayer === player1) ? player2 : player1
+            // const currentPlayer = chatId
+            const anotherPlayer = (chatId === player1) ? player2 : player1
+            const anotherSign = (currentPlayer === player1) ? 2 : 1
             if (checkIfWin(scene, wins, sign)) { //Проверяем не победил ли пользователь
-                bot.sendMessage(chatId, 'Вы победили, поздравляю!', {
-                    "reply_markup": {
-                        "inline_keyboard": renderButtons(scene) //конец игры
-                    },
-                })
-                userPoints += 1
-                bot.sendMessage(chatId, getPoints())
-                bot.sendMessage(chatId === player1?player2:player1, 'Вы проиграли, повезёт в следующий раз', {
-                    "reply_markup": {
-                        "inline_keyboard": renderButtons(scene) //конец игры
-                    },
-                })
+                sendTurn(chatId, 'Вы победили, поздравляю!', scene)
+                sendTurn(anotherPlayer, 'Вы проиграли, повезет в следующий раз', scene)
                 reset()
                 return false
             }
             const [freeX, freeY] = getFreeRandomCoords(scene)
             if (freeX === -1 && freeY === -1) {
-                bot.sendMessage(player2, 'Ничья!', {
-                    "reply_markup": {
-                        "inline_keyboard": renderButtons(scene) //конец игры
-                    },
-                })
-                bot.sendMessage(player1, 'Ничья!', {
-                    "reply_markup": {
-                        "inline_keyboard": renderButtons(scene) //конец игры
-                    },
-                })
+                sendTurn(chatId, 'Ничья!', scene)
+                sendTurn(anotherPlayer, 'Ничья!', scene)
                 reset()
                 return false
             }
-            bot.sendMessage(currentPlayer, 'Ожидаем ход противника', {
-                "reply_markup": {
-                    "inline_keyboard": renderButtons(scene, sign, 'd')
-                },
-            })
-            console.log({anotherPlayer})
-            bot.sendMessage(anotherPlayer, 'Ваш ход', {
-                "reply_markup": {
-                    "inline_keyboard": renderButtons(
-                        scene,
-                        (currentPlayer === player1) ? 2 : 1,
-                        'p'
-                    )
-                },
-            })
-
+            sendTurn(chatId, 'Ожидаем ход противника', scene)
+            sendTurn(anotherPlayer, 'Ваш ход', scene, anotherSign, 'p')
         }
 
         if (prefix === 'c') {
@@ -234,65 +116,45 @@ bot.on('callback_query', (query) => {
 
             // Получаем ход пользователя
             const [, x, y, strsign] = query.data.split(',')
+            const sign = +strsign
+            const botsign = sign === 1 ? 2 : 1
             if (scene[x][y] !== 0) {
                 bot.sendMessage(chatId, 'Так ходить нельзя, выберите другую клетку')
                 return false
             }
-            const sign = +strsign
-            console.log(x, y)
             scene[x][y] = sign //Ставим значок пользователя
             if (checkIfWin(scene, wins, sign)) { //Проверяем не победил ли пользователь
-                bot.sendMessage(chatId, 'Вы победили, поздравляю!', {
-                    "reply_markup": {
-                        "inline_keyboard": renderButtons(scene) //конец игры
-                    },
-                })
-                userPoints += 1
-                bot.sendMessage(chatId, getPoints())
+                sendTurn(chatId, 'Вы победили, поздравляю!', scene)
                 reset()
                 return false
             }
             // Получаем ход бота абсциссу ординату
             const [botX, botY] = getFreeRandomCoords(scene)
             if (botX === -1 && botY === -1) {
-                bot.sendMessage(chatId, 'Ничья!', {
-                    "reply_markup": {
-                        "inline_keyboard": renderButtons(scene) //конец игры
-                    },
-                })
-                bot.sendMessage(chatId, getPoints())
+                sendTurn(chatId, 'Ничья!', scene)
                 reset()
                 return false
             }
-            const botsign = sign === 1 ? 2 : 1
             scene[botX][botY] = botsign //Ставим нолик
             if (checkIfWin(scene, wins, botsign)) {  //Проверяем не победил ли бот
-                bot.sendMessage(chatId, 'Вы проиграли, повезёт в следующий раз', {
-                    "reply_markup": {
-                        "inline_keyboard": renderButtons(scene) //конец игры
-                    },
-                })
-                botPoints += 1
-                bot.sendMessage(chatId, getPoints())
+                sendTurn(chatId, 'Вы проиграли, повезёт в следующий раз', scene)
+                reset()
+                return false
+            }
+            const [botX2, botY2] = getFreeRandomCoords(scene)
+            if (botX2 === -1 && botY2 === -1) {
+                sendTurn(chatId, 'Ничья!', scene)
                 reset()
                 return false
             }
             //Предлагаем пользователю сделать ход
-            bot.sendMessage(chatId, 'Выполните ход!', {
-                "reply_markup": {
-                    "inline_keyboard": renderButtons(scene, sign)
-
-                },
-
-            });
+            sendTurn(chatId, 'Выполните ход!', scene, sign)
         }
     } catch (error) {
         console.log(error)
     }
-
-    // bot.sendMessage(chatId, str)
 })
-bot.on('message',(msg)=>{
+bot.on('message', (msg) => {
     console.log(msg)
 })
 
